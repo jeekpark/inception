bf2f3cf02642be9129bd174423cf04779052cfcd
jeekpark <jeekpark@student.42seoul.kr>
2024-04-26 02:52:09 +0900

    Fix

--- srcs/requirements/db/init.sh
+++ srcs/requirements/db/init.sh
@@ -3,10 +3,8 @@
 chown -R mysql:mysql /var/lib/mysql
 
 service mariadb start
-while ! mysqladmin ping -h localhost --silent; do
-    sleep 1
-done
-
+sleep 0.1 
+echo "$DB_NAME $DB_USER, $DB_PASSWORD, $DB_HOST"
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > init.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> init.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> init.sql
@@ -15,9 +13,7 @@ echo "FLUSH PRIVILEGES;" >> init.sql
 mysql < init.sql
 
 service mariadb stop
-while mysqladmin ping -h localhost --silent; do
-    sleep 1
-done
+sleep 0.1 
 
 
 mariadbd
--- srcs/requirements/http/init.sh
+++ srcs/requirements/http/init.sh
@@ -11,13 +11,16 @@ echo "
 server
 {
 	listen 443 ssl;
-
+	ssl_protocols  TLSv1.3;
 	ssl_certificate ${SSL_CERT};
 	ssl_certificate_key ${SSL_KEY};
-	ssl_protocols TLSv1.3;
+
 	root /var/www/html;
 	index index.php index.html index.htm;
 	server_name ${DOMAIN_NAME};
+" > /etc/nginx/sites-available/default
+
+echo '
 	location / {
 		try_files $uri $uri/ =404;
 	}
@@ -27,6 +30,6 @@ server
 		fastcgi_param SCRIPT_FILENAME /var/www/html/$fastcgi_script_name;
 		fastcgi_pass wp:9000;
 	}
-} " > /etc/nginx/sites-available/default
+} ' >> /etc/nginx/sites-available/default
 
 nginx -g "daemon off;"
